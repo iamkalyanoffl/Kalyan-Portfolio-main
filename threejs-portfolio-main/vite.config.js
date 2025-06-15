12f7faef5998e0ca6fef40dc606adac003bdea3f
Kalyan <kalyancr72005@gmail.com>
2025-06-15 16:54:17 +0530

    Fix: Set base path for Vercel

--- threejs-portfolio-main/vite.config.js
+++ threejs-portfolio-main/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Kalyan-Portfolio-main/', // <-- Notice the starting and ending slashes
+  base: '/', // <-- Notice the starting and ending slashes
   plugins: [react()],
 });
